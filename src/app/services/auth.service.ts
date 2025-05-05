c703fb1f033664f8066fb09d187808fa82f1be55
GrahamOMalley <gomgomgom@gmail.com>
2025-05-05 17:39:33 +0100

    fixing the user saving to the db on signin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/fire/auth';
 import { setPersistence } from 'firebase/auth';
 import { from, Observable } from 'rxjs';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -18,8 +19,7 @@ import { from, Observable } from 'rxjs';
 export class AuthService {
   user$: Observable<User | null>;
 
-  constructor(private firebaseAuth: Auth) {
-    // this.setSessionStoragePersistence();
+  constructor(private firebaseAuth: Auth, private firestore: Firestore) {
     this.user$ = user(this.firebaseAuth);
   }
 
@@ -36,6 +36,16 @@ export class AuthService {
       if (!user) {
         throw new Error('Google-Login error');
       }
+
+      // Save user to Firestore
+      const userRef = doc(this.firestore, `users/${user.uid}`);
+      await setDoc(userRef, {
+        uid: user.uid,
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        lastLogin: new Date().toISOString(),
+      }, { merge: true }); // Use merge to avoid overwriting existing data
     } catch (error) {
       console.error('Google-Login error:', error);
       throw error;
